3746cc658fe8aee9aa29039aae5718f435bdbdd7
franko88bit <franks188@icloud.com>
2023-07-21 19:05:54 -0400

    updated server configurations

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,11 @@ const morgan = require('morgan')
 
 const app = express();
 
-//Routes
+// Routes
 const home_route = require('./routers/home');
 const v1_route = require('./routers/api_v1');
 
-//middlewares
+// Middlewares
 app.use(cors());
 app.use(morgan('tiny'))
 app.use(express.json());
@@ -17,11 +17,11 @@ app.use('/',home_route);
 app.use('/api/v1',v1_route)
 
 app.all('*',(req,res)    => {
-  res.status(404).json({success:false,data:"resource not found"});
+  res.status(404).json({success:false,data:"resource not found!!!"});
 });
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT,()=>{
-    console.log(`Server started on PORT : ${PORT}`);
+    console.log(`Server runs on PORT : ${PORT}`);
 });
